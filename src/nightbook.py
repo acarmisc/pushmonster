73329777d57d2fc952792d0a3a6c3c0332f291a5
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2015-12-15 16:12:39 +0100

    removed some files

--- src/nightbook.py
+++
@@ -1,7 +0,0 @@
-class Nightbook(object):
-
-    def __init__(self, url):
-        self.base_url = url
-
-    def status(self):
-        return "%s/%s" % (self.base_url, '/api/commons/status/')
--- src/shared.py
+++
@@ -1,8 +0,0 @@
-class Responder(object):
-
-    def OK(self, payload, msg=None):
-        response = dict()
-        response['payload'] = payload
-        response['msg'] = msg or 'Success'
-
-        return response
